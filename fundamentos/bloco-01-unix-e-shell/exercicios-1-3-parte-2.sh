fad60521c11495baa03b9bbf2b2f7a33f3415c99
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-06-11 10:38:13 -0300

    exercicios do 1 ao 7

---
+++ fundamentos/bloco-01-unix-e-shell/exercicios-1-3-parte-2.sh
@@ -0,0 +1,20 @@
+# exercício 1
+curl -o countries.txt "https://gist.githubusercontent.com/kalinchernev/486393efcca01623b18d/raw/daa24c9fea66afb7d68f8d69f0c4b8eeb9406e83/countries"
+
+# exercício 2
+cat countries.txt
+
+# exercício 3
+less countries.txt
+
+# exercício 4
+less countries.txt
+
+# exercício 5
+grep Brazil countries.txt
+
+# exercício 6
+grep -i brazil countries.txt
+
+# exercício 7
+grep -v fox phrases.txt
