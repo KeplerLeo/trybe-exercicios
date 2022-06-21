488ddba61bc996673b08992bf0dc45c9660fba9b
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-06-21 17:28:19 -0300

    exercicio 5

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio5.js
@@ -0,0 +1,15 @@
+// Exercício 5
+let anguloA = 40;
+let anguloB = 40;
+let anguloC = 100;
+
+if (
+  anguloA + anguloB + anguloC == 180 &&
+  anguloA > 0 &&
+  anguloB > 0 &&
+  anguloC > 0
+) {
+  console.log(true);
+} else {
+  console.log(false + " Angulo incorreto");
+}
