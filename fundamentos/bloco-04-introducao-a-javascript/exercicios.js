36a52ec736bae2b18e6bda33b85684b3d18c161a
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-06-21 17:12:50 -0300

    exercicios 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicios.js
@@ -0,0 +1,18 @@
+// Exercício 1
+const a = 10;
+const b = 20;
+console.log("Exercício 1!");
+console.log("Soma: " + (a + b));
+console.log("Subtração: " + (a - b));
+console.log("Multiplicação: " + a * b);
+console.log("Divisão: " + a / b);
+console.log("Módulo: " + (a % b));
+console.log("------------------------");
+
+// Exercício 2
+console.log("Exercício 2!");
+if (a > b) {
+  console.log("'A' é maior que 'B'");
+}
+console.log("'B' é maior que 'A'");
+console.log("------------------------");
