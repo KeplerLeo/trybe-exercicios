1c234171314d000cef8d20c04eeff0a59ba0790a
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-06-22 18:13:11 -0300

    exercicios 8 e 9

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia2/exercicio8-9.js
@@ -0,0 +1,15 @@
+// Exercício 8
+console.log('**** Exercício 8 ****');
+let numbers = [];
+for (let i = 1; i < 26; i++) {
+  numbers.push(i);
+}
+console.log(numbers);
+console.log('---------------------');
+
+//Exercício 9
+console.log('**** Exercício 9 ****');
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] / 2)
+}
+console.log('---------------------');
