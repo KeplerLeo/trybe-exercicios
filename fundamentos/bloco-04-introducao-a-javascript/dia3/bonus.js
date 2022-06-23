8e29e531868855864cdc3fb48e06c614c50550df
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-06-23 20:09:47 -0300

    exercicios bonus 1, 2, 3 e 6

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia3/bonus.js
@@ -0,0 +1,47 @@
+// Bônus 1
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let quadrado = '';
+  for (let j = 0; j < n; j++) {
+    quadrado += '*';
+  }
+  console.log(quadrado);
+}
+
+// Bônus 2
+for (let i = 0; i < n; i++) {
+  let triangulo = '';
+  for (let j = 0; j <= i; j++) {
+    triangulo += '*';
+  }
+  console.log(triangulo);
+}
+
+// Bônus 3
+for (let i = 0; i < n; i++) {
+  let triangulo = '';
+  for (let j = 0; j < n; j++) {
+    if (j < n - i - 1) {
+      triangulo += ' ';
+    } else {
+      triangulo += '*';
+    }
+  }
+  console.log(triangulo);
+}
+
+// Bônus 4
+// Bônus 5
+// Bônus 6
+let num = 60;
+let divisor = 0;
+for (let i = 1; i <= num; i++) {
+  if (num % i === 0) {
+    divisor++;
+  }
+}
+if (divisor === 2) {
+  console.log('O número ' + num + ' é primo');
+} else {
+  console.log('O número ' + num + ' não é primo');
+}
